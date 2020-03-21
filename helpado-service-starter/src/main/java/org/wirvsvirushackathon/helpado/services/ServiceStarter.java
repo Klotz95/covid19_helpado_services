1bceae081972d7a4d450c6c696e6309e620481e4
NKO <NicoKotlenga@live.de>
2020-03-21 19:37:57 +0100

    user-storage: User Service
    - Added user service to jersey

--- helpado-service-starter/src/main/java/org/wirvsvirushackathon/helpado/services/ServiceStarter.java
+++ helpado-service-starter/src/main/java/org/wirvsvirushackathon/helpado/services/ServiceStarter.java
@@ -4,6 +4,11 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wirvsvirushackathon.helpado.services.user.UserService;
+import org.wirvsvirushackathon.helpado.session.SessionManager;
+import org.wirvsvirushackathon.helpado.session.SessionManagerImpl;
+import org.wirvsvirushackathon.helpado.user.storage.UserStorage;
+import org.wirvsvirushackathon.helpado.user.storage.UserStorageMockImpl;
 
 /**
  * This class will startup the following specified services:
@@ -19,11 +24,17 @@ public class ServiceStarter extends Application<ServiceConfiguration> {
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceStarter.class);
 
+
     public static void main(String[] args) throws Exception {
         new ServiceStarter().run(args);
     }
 
     public void run(ServiceConfiguration serviceConfiguration, Environment environment) throws Exception {
+        UserStorage userStorage = new UserStorageMockImpl();
+        SessionManager sessionManager = new SessionManagerImpl();
 
+        UserService userService = new UserService(userStorage,sessionManager);
+
+        environment.jersey().register(userService);
     }
 }
