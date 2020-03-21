086b802d23e6de2af70c55204b7a9a8bc3867cf4
BumseBine <mauritz.funke@gmail.com>
2020-03-21 10:26:57 +0100

    Added a constructor for the OrderStorage

--- helpado-order-storage/src/main/java/org/wirvsvirushackathon/order/storage/IOrderStorage.java
+++ helpado-order-storage/src/main/java/org/wirvsvirushackathon/order/storage/IOrderStorage.java
@@ -1,4 +1,8 @@
 package org.wirvsvirushackathon.order.storage;
 
+import java.util.Date;
+import java.util.List;
+
 public interface IOrderStorage {
+    public List<OrderStorage> getOrders();
 }
--- helpado-order-storage/src/main/java/org/wirvsvirushackathon/order/storage/OrderStorage.java
+++ helpado-order-storage/src/main/java/org/wirvsvirushackathon/order/storage/OrderStorage.java
@@ -1,4 +1,37 @@
 package org.wirvsvirushackathon.order.storage;
 
-public class OrderStorage {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class OrderStorage implements IOrderStorage {
+    public static List<OrderStorage> allOrders = new ArrayList<OrderStorage>();
+
+    public String orderId;
+    public String title;
+    public String description;
+    public float budget;
+    public Date createdAt;
+    public String createdBy;
+    public String assignedTo;
+    public List<String> orderedItems; // Use strings until items are implemented
+    public String state; // Use a string until OrderStates are implemented
+    public Date estimatedDeliveryTime;
+    public int rating;
+
+    public OrderStorage(String title, String description, float budget, Date createdAt, String createdBy, List<String> orderedItems, String state) {
+        this.title = title;
+        this.description = description;
+        this.budget = budget;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.orderedItems = orderedItems;
+        this.state = state;
+        allOrders.add(this);
+    }
+
+    public List<OrderStorage> getOrders() {
+        return allOrders;
+    }
 }
