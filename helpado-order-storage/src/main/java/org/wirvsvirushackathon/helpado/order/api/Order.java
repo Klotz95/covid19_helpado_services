c2c9e6d447e7df5b81bfca0d1eb8ccbf19331be2
NKO <NicoKotlenga@live.de>
2020-03-22 11:48:56 +0100

    order-service-enhancement: changed Order model
    - added json parameter name (WIP)

--- helpado-order-storage/src/main/java/org/wirvsvirushackathon/helpado/order/api/Order.java
+++ helpado-order-storage/src/main/java/org/wirvsvirushackathon/helpado/order/api/Order.java
@@ -1,27 +1,47 @@
 package org.wirvsvirushackathon.helpado.order.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 import java.util.List;
 
 public class Order {
+
+    private static final String ORDER_ID_PARAMETER_NAME = "orderId";
+    private static final String ORDERED_ITEMS_PARAMETER_NAME = "orderedItems";
+    private static final String LATEST_DELIVERY_WISHED_PARAMETER_NAME = "latestDeliveryWished";
+    private static final String ESTIMATED_DELIVERY_AT_PARAMETER_NAME = "estimatedDeliveryAt";
+    private static final String CREATED_AT_PARAMETER_NAME = "createdAt";
+    private static final String CREATED_BY_USER_ID_PARAMETER_NAME = "createdByUserId";
+    private static final String ASSIGNED_TO_USER_ID_PARAMETER_NAME = "assignedToUserId";
+    private static final String BUDGET_PARAMETER_NAME = "budget";
+    private static final String STATE_PARAMETER_NAME = "state";
+
+    @JsonProperty(ORDER_ID_PARAMETER_NAME)
     private String orderId;
+    @JsonProperty(ORDERED_ITEMS_PARAMETER_NAME)
     private List<OrderItem> orderedItems;
-    private Boolean premiumProducts;
+    @JsonProperty(LATEST_DELIVERY_WISHED_PARAMETER_NAME)
     private Date latestDeliveryWished;
+    @JsonProperty(ESTIMATED_DELIVERY_AT_PARAMETER_NAME)
     private Date estimatedDeliveryAt;
+    @JsonProperty(CREATED_AT_PARAMETER_NAME)
     private Date createdAt;
+    @JsonProperty(CREATED_BY_USER_ID_PARAMETER_NAME)
     private String createdByUserId;
+    @JsonProperty(ASSIGNED_TO_USER_ID_PARAMETER_NAME)
     private String assignedToUserId;
+    @JsonProperty(BUDGET_PARAMETER_NAME)
     private Float budget;
+    @JsonProperty(STATE_PARAMETER_NAME)
     private OrderState state;
 
     public Order() {
     }
 
-    public Order(String orderId, List<OrderItem> orderedItems, Boolean premiumProducts, Date latestDeliveryWished, Date estimatedDeliveryAt, Date createdAt, String createdByUserId, String assignedToUserId, Float budget, OrderState state) {
+    public Order(String orderId, List<OrderItem> orderedItems, Date latestDeliveryWished, Date estimatedDeliveryAt, Date createdAt, String createdByUserId, String assignedToUserId, Float budget, OrderState state) {
         this.orderId = orderId;
         this.orderedItems = orderedItems;
-        this.premiumProducts = premiumProducts;
         this.latestDeliveryWished = latestDeliveryWished;
         this.estimatedDeliveryAt = estimatedDeliveryAt;
         this.createdAt = createdAt;
@@ -47,13 +67,7 @@ public class Order {
         this.orderedItems = orderedItems;
     }
 
-    public Boolean getPremiumProducts() {
-        return premiumProducts;
-    }
 
-    public void setPremiumProducts(Boolean premiumProducts) {
-        this.premiumProducts = premiumProducts;
-    }
 
     public Date getLatestDeliveryWished() {
         return latestDeliveryWished;
